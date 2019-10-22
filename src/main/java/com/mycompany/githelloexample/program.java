aae86c1b827fad93ba794f617a4da8e4b6cd3540
pan li <1904782@uwtsd.ac.uk>
2019-10-22 10:57:38 +0000

    Create hello world example

---
+++ src/main/java/com/mycompany/githelloexample/program.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.githelloexample;
+
+import java.util.*;
+/**
+ *
+ * @author uwtsd-student
+ */
+public class program {
+    public static void main(String[] args){
+    System.out.println("helloworld");
+    
+    }
+}
